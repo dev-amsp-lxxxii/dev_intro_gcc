79500ff5222f519c919809f9ec30eed81b167e18
Adelmo Menezes da Silva Protázio <adelmo.msp@gmail.com>
2024-10-24 16:49:40 -0300

    Curso de Programação C -> Aula I - Como fazer comentários em meu código C

---
+++ aula_0001/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    Aula: 002
+    Tema: Como fazer comentários em meu código C
+    Link: https://www.youtube.com/watch?v=yrWIlIVZHB0&list=PLqJK4Oyr5WSjjEQCKkX6oXFORZX7ro3DA&index=2
+    Data: 24.10.2024
+*/
+
+int main()
+{
+    /*
+        Isso aqui é
+        um grande bloco
+        de comentário
+    */
+    printf("Aprendendo comentários na linguagem C.\n");
+    // printf("Hello world!\n");
+    return 0;
+}
