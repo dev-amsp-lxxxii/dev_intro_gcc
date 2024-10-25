bfc9c930c63db0444e33314a6df3a40de17ee0f8
Adelmo Menezes da Silva Protázio <adelmo.msp@gmail.com>
2024-10-25 16:03:34 -0300

    Curso de Programação C -> Aula VI - Como criar variáveis e ler caractres com a função getchar

---
+++ aula_0007/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    Aula: 0007
+    Tema: Como criar variáveis e ler caracteres com a função getchar
+    Link: https://www.youtube.com/watch?v=NgffM1_fdHo&list=PLqJK4Oyr5WSjjEQCKkX6oXFORZX7ro3DA&index=8
+    Data: 25.10.2024
+*/
+
+int main()
+{
+    char letra;
+    printf("Digite uma letra: ");
+    letra = getchar(); // ira pegar o caracter no buffer do teclado e armazenar
+    printf("Caracter lido: %c", letra);
+    return 0;
+}
